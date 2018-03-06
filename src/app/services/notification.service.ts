690679d991c3e28b8ca4973ff111ff408cc79695
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2018-03-06 16:27:56 +0100

    some ui improvementss

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.staging';
 import { api } from '../../environments/environment.api';
 import { IJSONResponse } from './jsonResponse.interface';
+import { Observable } from 'rxjs/Observable';
 
 export interface IP {
   origin: string;
